9bc40c221a9f698b8348bbce29470cfb080c4ad7
Lukas Stejskal <stejskal@ataxo.com>
2011-02-13 13:47:15 +0100

    remove active record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# use rails without active record
+# require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# TODO do we have to user active resource?
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+# TODO what about active support?
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,9 +25,6 @@ module Icanhazblog
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
