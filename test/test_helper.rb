a37d41ca965334b09183fef093e832871117c53d
Lukas Stejskal <stejskal@ataxo.com>
2011-02-14 16:48:25 +0100

    fix factories loading during tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,11 @@ require 'rails/test_help'
 require 'shoulda'
 require 'factory_girl'
 
-# load fixtures
-Dir[ File.join(File.dirname(__FILE__), 'factories/*.rb') ].each { |f| require f }
+# load factories
+# PS: comment and tag factories have to be loaded before article factory
+%w{ user comment tag article }.each do |filename|
+  require File.join(File.dirname(__FILE__), 'factories', "#{filename}_factory")
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
