2260d473efaa3882bcbb39d53104eaa6f7ff465c
Lukas Stejskal <stejskal@ataxo.com>
2011-02-14 19:51:03 +0100

    refactor user model tests to shoulda

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,66 +5,81 @@ class UserTest < ActiveSupport::TestCase
   setup do
     @user = Factory.build(:user)
   end
-
+  
   teardown do
     User.all.each { |u| u.destroy }
   end
 
-  test "should not save without email" do
-    @user.email = nil
-    assert ! @user.save
+  context "When creating User, it" do
+
+    should "not save without email" do
+      @user.email = nil
+      assert ! @user.save
+    end
+
+    should "not save without password" do
+      @user.password = nil
+      assert ! @user.save
+    end
+
+    should "not save without nickname" do
+      @user.nickname = nil
+      assert ! @user.save
+    end
+    
+    should "save only unique emails" do
+      @user.save
+      another_user = Factory.build(:user)
+      another_user.email = @user.email
+      assert ! another_user.save
+    end
+  
+    should "save only unique nicknames" do
+      @user.save
+      another_user = Factory.build(:user)
+      another_user.nickname = @user.nickname
+      assert ! another_user.save    
+    end
+  
+    should "save admin attribute as false by default" do
+      user = Factory.create(:user, :admin => nil)
+      assert ! user.admin?
+    end
+    
+    should "save with valid attributes" do
+      assert @user.save
+    end
+    
   end
 
-  test "should not save without password" do
-    @user.password = nil
-    assert ! @user.save
-  end
+  context "User.find_by_email" do
 
-  test "should not save without nickname" do
-    @user.nickname = nil
-    assert ! @user.save
+    should "find user with existing email" do
+      @user.save
+      assert_not_nil User.find_by_email(@user.email)
+    end
+  
+    should "not find user with non-existent email" do
+      assert_nil User.find_by_email("non-existent email")
+    end
   end
   
-  test "should save only unique emails" do
-    @user.save
-    another_user = Factory.build(:user)
-    another_user.email = @user.email
-    assert ! another_user.save
-  end
-
-  test "should save only unique nicknames" do
-    @user.save
-    another_user = Factory.build(:user)
-    another_user.nickname = @user.nickname
-    assert ! another_user.save    
-  end
-
-  test "should save admin attribute as false by default" do
-    user = Factory.create(:user, :admin => nil)
-    assert ! user.admin?
-  end
+  context "User.authenticate " do
+    setup do
+      @user = Factory.create(:user)
+    end
+    
+    teardown do
+      @user.destroy
+    end
   
-  test "should save with valid attributes" do
-    assert @user.save
-  end
-
-  test "should find user with existing email" do
-    @user.save
-    assert_not_nil User.find_by_email(@user.email)
-  end
-
-  test "should not find user with non-existent email" do
-    assert_nil User.find_by_email("non-existent email")
-  end
+    should "return nil on invalid authentication data" do
+      assert_nil User.authenticate(@user.email, @user.password.reverse)    
+    end
   
-  test "should be authenticated with valid password" do
-    @user.save
-    assert_not_nil User.authenticate(@user.email, @user.password)
-  end
-
-  test "should not be authenticated with invalid password" do
-    @user.save
-    assert_nil User.authenticate(@user.email, @user.password.reverse)    
+    should "return User instance valid authentication data" do
+      assert_equal User, User.authenticate(@user.email, @user.password).class
+    end  
   end
 
 end
