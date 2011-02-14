e7230d20736c597844ef68626277163134a212a8
Lukas Stejskal <stejskal@ataxo.com>
2011-02-14 22:28:18 +0100

    add basic validations and tests for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,15 @@ class Comment
   
   field :title
   field :content
-  field :user_nickname
+  field :user_nickname, :default => "Anonymous"
   field :user_location
 
   embedded_in :article, :inverse_of => :comments
+
+  validates_presence_of :title, :content
+  validates_length_of :title, :maximum => 100
+  validates_length_of :content, :maximum => 500
+  validates_length_of :user_nickname, :maximum => 50
+  validates_length_of :user_location, :maximum => 50
+
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,8 +1,50 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "When creating Comment, it" do
+
+    setup do
+      @comment = Factory.build(:comment)
+    end
+    
+    teardown do
+      @comment.destroy
+    end
+
+    should "not save without title" do
+      @comment.title = nil
+      assert ! @comment.save
+    end
+
+    should "not save with content" do
+      @comment.content = nil
+      assert ! @comment.save
+    end
+
+    should "not save with title over max. length" do
+      @comment.title = random_string(101)
+      assert ! @comment.save
+    end
+
+    should "not save with content over max. length" do
+      @comment.content = random_string(501)
+      assert ! @comment.save
+    end
+
+    should "not save with user_nickname over max. length" do
+      @comment.user_nickname = random_string(51)
+      assert ! @comment.save
+    end
+
+    should "not save with user_location over max. length" do
+      @comment.user_location = random_string(51)
+      assert ! @comment.save
+    end
+
+    should "save with valid data" do
+      assert @comment.save
+    end
+
   end
 end
