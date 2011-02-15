ef4bcd9b1f1392834f42e2da00eeb75c4df0fac4
Lukas Stejskal <stejskal@ataxo.com>
2011-02-15 12:21:55 +0100

    remove automatic generating of comments and tags in Article factory

--- test/factories/article_factory.rb
+++ test/factories/article_factory.rb
@@ -1,14 +1,11 @@
 
-require_relative 'comment_factory'
-require_relative 'tag_factory'
-
 Factory.define :article, :class => Article do |document|
   document.sequence(:title)       { |n| "Article #{n}" }
   document.content                { |d| "This is a content for #{d.title}." }
   document.visible                false
   document.published_at           nil
-
-  document.comments               5.times.map { Factory.create(:comment) } 
-  document.tags                   3.times.map { Factory.create(:tag) } 
+  
+  document.comments               []
+  document.tags                   []
 end
 
