ff1f310645da0d5b6d212e37cb69b34d87aa8faa
Lukas Stejskal <stejskal@ataxo.com>
2011-02-15 23:44:57 +0100

    add published_article factory

--- test/factories/article_factory.rb
+++ test/factories/article_factory.rb
@@ -9,3 +9,13 @@ Factory.define :article, :class => Article do |document|
   document.tags                   []
 end
 
+Factory.define :published_article, :class => Article do |document|
+  document.sequence(:title)       { |n| "Article #{n}" }
+  document.content                { |d| "This is a content for #{d.title}." }
+  document.visible                true
+  document.published_at           Time.now
+  
+  document.comments               []
+  document.tags                   []
+end
+
