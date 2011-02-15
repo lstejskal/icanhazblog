c8f7327a9bed6b280f8df1f77d71edb4807aff6e
Lukas Stejskal <stejskal@ataxo.com>
2011-02-15 14:58:28 +0100

    implement basic articles listing in controller and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles
   def index
-    @articles = Article.all
+    @articles = Article.list(params)
 
     respond_to do |format|
       format.html # index.html.erb
