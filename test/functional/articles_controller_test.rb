6d34d60c13735a6fc62c60ad8b2171dbf41f8bf4
Lukas Stejskal <lucastej@gmail.com>
2011-02-17 14:19:45 +0100

    refactor article unit tests

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -7,21 +7,21 @@ class ArticlesControllerTest < ActionController::TestCase
 
   def teardown
     Article.all.each { |article| article.destroy }
-  end  
+  end
 
-  test "should get index" do
+  test "viewing list of articles" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:articles)
     assert_equal 1, assigns(:articles).size
   end
 
-  test "should show article with valid id" do
+  test "viewing article" do
     get :show, :id => @article.to_param
     assert_response :success
+    assert_equal @article.to_param, assigns(:article).to_param
   end
   
-  test "should return 404 if article can't be found" do    
+  test "viewing non-existent article" do    
     assert_raise(ActionController::RoutingError) { get :show, :id => "123" }  
   end
   
