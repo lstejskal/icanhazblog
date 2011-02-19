abec6f0b724989efd4c80c913ae65be80acb7501
Lukas Stejskal <lucastej@gmail.com>
2011-02-19 21:30:43 +0100

    add TODO note to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,15 +18,17 @@ class Article
   validates_length_of :title, :minimum => 3, :maximum => 250
   
   # provide public interface for listing and searching articles
+  # TODO convert to separate model, ArticleSearch?
   #
   def self.list(params = {})    
     params = self.sanitize_search_params(params)
     
     params[:page] ||= 1
     params[:per_page] ||= 10
+    # TODO return errors on invalid dates - after conversion into ArticleSearch model
     params[:from] = (Date.parse(params[:from]) rescue nil) if params[:from]
     params[:to] = (Date.parse(params[:to]) rescue nil) if params[:to]
-    
+
     q = self
     
     # show only visible articles
