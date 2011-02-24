d5e7ac2a1a743a87bebdb86ac42c870839e7b4e6
Lukas Stejskal <lucastej@gmail.com>
2011-02-24 19:00:52 +0100

    move article controller test to shoulda

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -10,48 +10,52 @@ class ArticlesControllerTest < ActionController::TestCase
     Article.all.each { |article| article.destroy }
   end
 
-  test "viewing list of articles" do
-    get :index
-    assert_response :success
-    assert_equal 3, assigns(:articles).size
-  end
+  context "user" do 
 
-  test "filtering articles by tag" do
-    get :index, :tag => "ruby"
-    assert_response :success
-    assert_equal 2, assigns(:articles).size
-  end
+    should "view list of articles" do
+      get :index
+      assert_response :success
+      assert_equal 3, assigns(:articles).size
+    end
 
-  test "filtering articles by non-existent tag" do
-    get :index, :tag => "assembler"
-    assert_response :success
-    assert_equal 0, assigns(:articles).size
-  end
+    should "filter articles by tag" do
+      get :index, :tag => "ruby"
+      assert_response :success
+      assert_equal 2, assigns(:articles).size
+    end
 
-  test "filtering articles by date range" do
-    get :index, :from => '05.02.2011', :to => '09.02.2011'
+    should "not get any articles when filtering by non-existent tag" do
+      get :index, :tag => "assembler"
+      assert_response :success
+      assert_equal 0, assigns(:articles).size
+    end
 
-    assert_response :success
-    assert_equal 1, assigns(:articles).size
-  end
-
-  test "filtering articles by invalid date range" do
-    get :index, :from => '08.20.2011', :to => '10.02.2011'
- 
-    assert_response :success
-    assert_equal 3, assigns(:articles).size
-    # PS: we won't allow user to enter date range directly
-    assert_nil flash.alert
-  end
-
-  test "viewing article" do
-    get :show, :id => @article.to_param
-    assert_response :success
-    assert_equal @article.to_param, assigns(:article).to_param
-  end
+    should "filter articles by date range" do
+      get :index, :from => '05.02.2011', :to => '09.02.2011'
   
-  test "viewing non-existent article" do    
-    assert_raise(ActionController::RoutingError) { get :show, :id => "123" }  
+      assert_response :success
+      assert_equal 1, assigns(:articles).size
+    end
+
+    should "get all articles when filtering by invalid date range" do
+      get :index, :from => '08.20.2011', :to => '10.02.2011'
+   
+      assert_response :success
+      assert_equal 3, assigns(:articles).size
+      # PS: we won't allow user to enter date range directly
+      assert_nil flash.alert
+    end
+
+    should "view article" do
+      get :show, :id => @article.to_param
+      assert_response :success
+      assert_equal @article.to_param, assigns(:article).to_param
+    end
+  
+    should "get 404 error when viewing non-existent article" do    
+      assert_raise(ActionController::RoutingError) { get :show, :id => "123" }  
+    end
+  
   end
   
 =begin
