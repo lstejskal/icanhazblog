5cda3930bfde6f84799de2e049333543aefe2260
Lukas Stejskal <lucastej@gmail.com>
2011-02-25 10:52:22 +0100

    refactor sessions controller test to shoulda

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
-  def setup
-    @user = Factory.create(:admin_user)
-  end
-    
-  def teardown
-    @user.destroy
-  end
-
   test "routing log_in -> new session" do
     assert_routing log_in_path, { :controller => "sessions", :action => "new" }
   end  
@@ -17,32 +9,44 @@ class SessionsControllerTest < ActionController::TestCase
   test "routing log_out -> destroy session" do
     assert_routing log_out_path, { :controller => "sessions", :action => "destroy" }
   end
-  
-  test "viewing login form" do
-    get :new
-    assert_response :success
-    assert_template :new
-  end
 
-  test "logging in with invalid password" do
-    post :create, :email => @user.email, :password => @user.password.reverse
-    assert_response :success
-    assert_template :new
-    assert_not_nil flash.alert
-  end
+  context "user" do
 
-  test "logging in with valid password" do
-    post :create, :email => @user.email, :password => @user.password
-    assert_redirected_to root_path
-    assert_not_nil flash.notice
-    assert_equal @user.to_param, session[:user_id]
-  end
+    setup do
+      @user = Factory.create(:admin_user)
+    end
+      
+    teardown do
+      @user.destroy
+    end
 
-  test "logging out" do
-    get :destroy
-    assert_redirected_to root_path
-    assert_not_nil flash.notice
-    assert ! session[:user_id]
+    should "view login form" do
+      get :new
+      assert_response :success
+      assert_template :new
+    end
+
+    should "not log in with invalid password" do
+      post :create, :email => @user.email, :password => @user.password.reverse
+      assert_response :success
+      assert_template :new
+      assert_not_nil flash.alert
+    end
+
+    should "log in with valid password" do
+      post :create, :email => @user.email, :password => @user.password
+      assert_redirected_to root_path
+      assert_not_nil flash.notice
+      assert_equal @user.to_param, session[:user_id]
+    end
+
+    should "log out" do
+      get :destroy
+      assert_redirected_to root_path
+      assert_not_nil flash.notice
+      assert ! session[:user_id]
+    end
+
   end
 
 end
