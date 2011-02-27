2b84e73699e7cb1e9aeb3d609abd547dcfe57d6d
Lukas Stejskal <lucastej@gmail.com>
2011-02-27 22:08:42 +0100

    update Tag model: remove Article->Tag embedding, add TODO

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,12 @@
+# this model is not used at the moment
+# TODO either remove Tag model or refactor it into relation to Article model
+#
 class Tag
   include Mongoid::Document
   include Mongoid::Timestamps
   
   field :name
-  
-  embedded_in :article, :inverse_of => :tags
-  
+    
   validates_presence_of :name
   validates_length_of :name, :minimum => 2, :maximum => 50
   validates_uniqueness_of :name
