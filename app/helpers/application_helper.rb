dfaf1213e13e2588be0f713824edbf6061e5d38a
Lukas Stejskal <lucastej@gmail.com>
2011-03-03 13:46:10 +0100

    add formatting of the published_at date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
     link_to name, articles_path( Article.sanitize_search_params( params.merge(search_params) ) )
   end
   
+  # display date
+  # format: day_of_the_week, month_name day_nr, year
+  # example: Thursday, March 03, 2011 
+  #
+  def formatted_date(date)
+    date.strftime("%A, %B %m, %Y")
+  end
 end
