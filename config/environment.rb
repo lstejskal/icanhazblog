640696782f5c9ebd717b41cae26571b44c5e93ae
Lukas Stejskal <lucastej@gmail.com>
2011-03-03 10:38:43 +0100

    fix problem with rubygems 1.5 and higher

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+# fix problem with rubygems 1.5 and higher
+# PS: 1.5 switches YAML engine which crashes on <<: in YAML config
+YAML::ENGINE.yamler= 'syck' 
 
 # Initialize the rails application
 Icanhazblog::Application.initialize!
