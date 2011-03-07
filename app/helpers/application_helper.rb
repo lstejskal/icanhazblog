225ab8c571d58da0097dc2d69e5a665befab5038
Lukas Stejskal <lucastej@gmail.com>
2011-03-07 14:13:15 +0100

    allow search_link_to method to accept link parameters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   
   # create link to article search
   #
-  def search_link_to(name, search_params)
-    link_to name, articles_path( Article.sanitize_search_params( params.merge(search_params) ) )
+  def search_link_to(name, search_params, link_params = {})
+    link_to name, articles_path( Article.sanitize_search_params( params.merge(search_params) ) ), link_params
   end
   
   # display date
