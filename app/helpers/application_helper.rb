3dd18fc9e5c6f4afa09d12076900c6eec34a463b
Lukas Stejskal <lucastej@gmail.com>
2011-03-28 21:39:19 +0200

    fix combined searches in sidebar - drop params from previous searches

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # create link to article search
   #
   def search_link_to(name, search_params, link_params = {})
-    link_to name, articles_path( Article.sanitize_search_params( params.merge(search_params) ) ), link_params
+    link_to name, articles_path( Article.sanitize_search_params(search_params) ), link_params
   end
   
   # display date
