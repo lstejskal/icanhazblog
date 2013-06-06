f52deaf1f1d2eb0e103b22585f5b4ad70ee4956a
Lukas Stejskal <lucastej@gmail.com>
2013-06-06 21:50:38 +0200

    Commented out factory girl initializer (for development only)

--- config/initializers/factories.rb
+++ config/initializers/factories.rb
@@ -1,6 +1,7 @@
 
 # load factories
 # PS: for test environment factories are loaded from test/test_helper
-if Rails.env.development?
-  Dir[ File.join(File.dirname(__FILE__), '../../test/factories/*.rb') ].each { |f| require f }
-end
+# TODO commented out, gotta update factory girl DSL first 
+#if Rails.env.development?
+#  Dir[ File.join(File.dirname(__FILE__), '../../test/factories/*.rb') ].each { |f| require f }
+#end
